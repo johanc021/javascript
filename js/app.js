4355ffd0a258b85b1ac33234de5ce5f4dd3568d0
johan021 <facjohan@hotmail.com>
2022-12-19 18:18:44 -0500

    Buscador Integrado

--- js/app.js
+++ js/app.js
@@ -644,56 +644,29 @@ const verificarGuardarEditado = (index) => {
 
 }
 
+// Obtener el campo de búsqueda y la tabla
+const campoDeBusqueda = document.getElementById('searchInput');
+const tabla = document.getElementById('tabla');
 
-document.getElementById("buscador").addEventListener("keyup", encontrando);
+campoDeBusqueda.addEventListener('keyup', (event) => {
+    const valorBuscado = event.target.value.toLowerCase();
 
-function encontrando() {
-    const buscandoTexto = document.getElementById('buscador').value.toLowerCase();
-    console.log(buscandoTexto)
-}
+    // Filtrar la tabla
+    const filas = tabla.querySelectorAll('tbody tr');
+    filas.forEach((row) => {
+        const celdas = row.querySelectorAll('td');
+        let match = false;
 
-
-
-
-
-
-
-/* function doSearch() {
-
-    const tableReg = document.getElementById('table');
-
-    const searchText = document.getElementById('buscador').value.toLowerCase();
-    console.log(searchText)
-
-    let total = 0;
-
-    // Recorremos todas las filas con contenido de la tabla
-
-    for (let i = 1; i < tableReg.rows.length; i++) {
-        // Si el td tiene la clase "noSearch" no se busca en su cntenido
-        if (tableReg.rows[i].classList.contains("noSearch")) {
-            continue;
-        }
-
-
-        let found = false;
-        const cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
-        // Recorremos todas las celdas
-        for (let j = 0; j < cellsOfRow.length && !found; j++) {
-
-            const compareWith = cellsOfRow[j].innerHTML.toLowerCase();
-            // Buscamos el texto en el contenido de la celda
-            if (searchText.length == 0 || compareWith.indexOf(searchText) > -1) {
-                found = true;
-                total++;
+        celdas.forEach((celda) => {
+            if (celda.textContent.toLowerCase().includes(valorBuscado)) {
+                match = true;
             }
-        }
-        if (found) {
-            tableReg.rows[i].style.display = '';
+        });
+
+        if (match) {
+            row.style.display = '';
         } else {
-            // si no ha encontrado ninguna coincidencia, esconde la
-            // fila de la tabla
-            tableReg.rows[i].style.display = 'none';
+            row.style.display = 'none';
         }
-    }
-} */
+    });
+});
